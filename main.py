e483e39620e8fae959179f5b12da9044f1d65f80
Thanh Ngoc 1 <phanthanhngoc11@gmail.com>
2022-10-26 10:10:27 +0700

    fix some bug

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from .src import create_app
+from src import create_app
+from os import getenv
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+    app.run(host='0.0.0.0', port=getenv('PORT'))
